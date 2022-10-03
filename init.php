71442cca35cde87a6de79b0670519b744c56ecc2
amstfn <114819834+amstfn@users.noreply.github.com>
2022-10-03 18:05:57 +0330

    Delete init.php

--- init.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <form action="Hi.php" method="post">
-            Name<input type="text" name="name">
-            Email<input type="text" name="email">
-            <input type="submit">
-        </form>
-    </body>
-</html>
